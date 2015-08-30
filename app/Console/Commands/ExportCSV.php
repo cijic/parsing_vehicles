95d537f599cd3dd9360bfd2fc7f11f08ea85a026
cijic <altcode@ya.ru>
2015-08-30 23:29:50 +0300

    ExportCSV: * Added option to define filename of exported file; * Removed spare and unused; * Removed usage of models;

--- app/Console/Commands/ExportCSV.php
+++ app/Console/Commands/ExportCSV.php
@@ -2,9 +2,6 @@
 
 namespace App\Console\Commands;
 
-use App\Models\ModelBrand;
-use App\Models\ModelModifications;
-use App\Models\ModelProperties;
 use DB;
 use Excel;
 use Illuminate\Console\Command;
@@ -16,7 +13,7 @@ class ExportCSV extends Command
      *
      * @var string
      */
-    protected $signature = 'parsing:export {table=properties}';
+    protected $signature = 'parsing:export {table=properties} {--filename=}';
 
     /**
      * The console command description.
@@ -27,8 +24,6 @@ class ExportCSV extends Command
 
     /**
      * Create a new command instance.
-     *
-     * @return void
      */
     public function __construct()
     {
@@ -43,112 +38,36 @@ class ExportCSV extends Command
     public function handle()
     {
         $table = $this->argument('table');
-        /*
-                $timeStart = microtime(true);
-                $data = $this->getData($table);
-                $timeEnd = microtime(true);
-                echo 'Data receiving time: ' . ($timeEnd - $timeStart) . PHP_EOL;
+        $filename = $table;
 
-                $timeStart = microtime(true);
-                $this->exportData($table, $data);
-                $timeEnd = microtime(true);
-                echo 'Exporting data time: ' . ($timeEnd - $timeStart) . PHP_EOL;
-        */
-        $timeStart = microtime(true);
-        $model = $this->getModel($table);
-        $timeEnd = microtime(true);
-        echo 'Data receiving time: ' . ($timeEnd - $timeStart) . PHP_EOL;
-        /*
-                $timeStart = microtime(true);
-                $this->exportModelData($table, $model);
-                $timeEnd = microtime(true);
-                echo 'Exporting data time: ' . ($timeEnd - $timeStart) . PHP_EOL;
-        */
+        if ($this->option('filename')) {
+            $filename = $this->option('filename');
+        }
 
         $timeStart = microtime(true);
-        $this->exportSequentially($table, $model);
+        $this->exportSequentially($table, $filename);
         $timeEnd = microtime(true);
         echo 'Exporting data time: ' . ($timeEnd - $timeStart) . PHP_EOL;
     }
 
-    private function getModel($table)
+    private function exportSequentially($table, $filename)
     {
-        $model = null;
+        Excel::create($filename, function ($excel) use ($table) {
+            $excel->sheet('sheet1', function ($sheet) use ($table) {
+                $count = DB::table($table)->count();
+                $step = 10;
 
-        switch ($table) {
-            case 'brand':
-                $model = new ModelBrand();
-                break;
+                for ($i = 0; $i < $count; $i += $step) {
+                    $resultRows = DB::table($table)->skip($i)->take($step)->get();
+                    $countResultRows = count($resultRows);
+                    $rows = [];
 
-            case 'properties':
-                $model = new ModelProperties();
-                break;
+                    for ($j = 0; $j < $countResultRows; $j++) {
+                        $rows[] = (array)$resultRows[$j];
+                    }
 
-            case 'modifications':
-                $model = new ModelModifications();
-                break;
-
-            default:
-                $model = new ModelProperties();
-                break;
-        }
-
-        return $model;
-    }
-
-    private function exportSequentially($table, $model)
-    {
-        Excel::create($table . '_seq_model', function ($excel) use ($model) {
-            $excel->sheet('sheet1', function ($sheet) use ($model) {
-                $count = $model::count();
-
-                $row = $model->first()->toArray();
-                $sheet->appendRow($row);
-
-                for ($i = 1; $i < $count; $i += 10) {
-                    $rows = $model->skip($i)->take(10)->get()->toArray();
                     $sheet->rows($rows);
                 }
-            });
-        })->store('csv', storage_path('export/csv'));
-    }
-
-    private function getData($table)
-    {
-        $rows = DB::table($table)->get();
-        $data = [];
-        $count = count($rows);
-
-        for ($i = 0; $i < $count; $i++) {
-            $row = $rows[$i];
-            $keys = array_keys(get_object_vars($row));
-
-            $result = [];
-
-            foreach ($keys as $key) {
-                $result[$key] = $row->$key;
-            }
-
-            $data[] = $result;
-        }
-
-        return $data;
-    }
-
-    private function exportData($table, $data)
-    {
-        Excel::create($table, function ($excel) use ($data) {
-            $excel->sheet('sheet1', function ($sheet) use ($data) {
-                $sheet->fromArray($data);
-            });
-        })->store('csv', storage_path('export/csv'));
-    }
-
-    private function exportModelData($table, $model)
-    {
-        Excel::create($table . '_model', function ($excel) use ($model) {
-            $excel->sheet('sheet1', function ($sheet) use ($model) {
-                $sheet->fromModel($model::all());
             });
         })->store('csv', storage_path('export/csv'));
     }
